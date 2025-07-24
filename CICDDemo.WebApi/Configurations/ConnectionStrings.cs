a6c59699f167d0601281fffdb91052bc3387b481
Julius Martin <juliusjoemarmartin@gmail.com>
2025-07-24 17:43:04 +0800

    feature(connectionstring): add connectionstring configuration
    add dependency injection class
    implement the di in program.cs

---
+++ CICDDemo.WebApi/Configurations/ConnectionStrings.cs
@@ -0,0 +1,7 @@
+namespace CICDDemo.WebApi.Configurations;
+
+public class ConnectionStrings
+{
+    public string? DefaultConnection { get; set; }
+    public string? SmtpConnection { get; set; }
+}
